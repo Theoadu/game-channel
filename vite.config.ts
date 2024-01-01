16d8587f967bf15312b6971a4ace081b5ddef307
Theophilus E.K Adukpo <theoadu@gmail.com>
2024-01-01 19:41:04 +0000

    Fetch the games

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// https://dev.to/boostup/uncaught-referenceerror-process-is-not-defined-12kg
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.RAWG_KEY': JSON.stringify(env.RAWG_KEY),
+    },
+    plugins: [react()],
+  };
+});
