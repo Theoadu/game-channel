3d57c8752247a7d5ef5bb7703af697363f3e6ee4
Theophilus E.K Adukpo <theoadu@gmail.com>
2024-01-01 20:54:48 +0000

    Displaying Platform Icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,42 @@
+import { IconType } from 'react-icons';
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaAndroid,
+  FaLinux,
+} from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { HStack, Icon } from '@chakra-ui/react';
+import { Platform } from '../hooks/useGames';
+
+type Props = {
+  platforms: Platform[];
+};
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+    linux: FaLinux,
+  };
+
+  return (
+    <HStack marginY={'10px'}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} key={platform.id} color='gray.500' />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
