34be9ba7ae3195d76ebfe9084972642f586c8325
Theophilus E.K Adukpo <theoadu@gmail.com>
2024-01-11 01:21:50 +0000

    use key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,8 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: process.env.RAWG_KEY,
+    key: '54fb8a42e6ba4f1a8749f6caa855e50a',
   },
 });
-
-console.log(process.env.RAWG_KEY);
